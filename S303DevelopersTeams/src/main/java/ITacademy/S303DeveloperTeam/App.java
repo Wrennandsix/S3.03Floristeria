772e701e3d91299276504483999cb1ad8212700f
Wrennandsix <marcsmoreno@hotmail.com>
2023-12-09 17:48:39 +0100

    starter code

---
+++ S303DevelopersTeams/src/main/java/ITacademy/S303DeveloperTeam/App.java
@@ -0,0 +1,13 @@
+package ITacademy.S303DeveloperTeam;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
