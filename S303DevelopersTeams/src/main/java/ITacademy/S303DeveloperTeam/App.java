e2d8981ba2da910872d18dc76e83ec4b694684ed
gerar <gerar@DESKTOP-3EAC44Q>
2023-12-09 19:59:14 +0100

    pull request test 3

--- S303DevelopersTeams/src/main/java/ITacademy/S303DeveloperTeam/App.java
+++ S303DevelopersTeams/src/main/java/ITacademy/S303DeveloperTeam/App.java
@@ -5,8 +5,9 @@ public class App {
 	
     public static void main( String[] args ) {
     	
-        System.out.println( "World!" );
-        System.out.println( "Nooooooooooo!" );
+        System.out.println("Bye World!" );
+        System.out.println("Nooooooooooo!" );
+        System.out.println("potatoe");
         
     }
 }
